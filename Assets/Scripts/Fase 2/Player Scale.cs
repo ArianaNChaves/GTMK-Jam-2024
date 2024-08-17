fc3ec36236646a2068dc088e2078c873e5e3ab93
ArianaNChaves <navi.ariana99@gmail.com>
2024-08-17 13:40:05 -0300

    Added player hit color

--- Assets/Scripts/Fase 2/Player Scale.cs
+++ Assets/Scripts/Fase 2/Player Scale.cs
@@ -10,6 +10,16 @@ public class PlayerScale : MonoBehaviour
     [SerializeField] private float reductionRate = 0.05f;
     [SerializeField] private float minRate = 0.05f;
 
+    
+    private Color _default;
+    private Color _hit;
+    private SpriteRenderer _spriteRenderer;
+    private void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _default = _spriteRenderer.color;
+        _hit = Color.white;
+    }
 
     private void OnEnable()
     {
@@ -23,9 +33,20 @@ public class PlayerScale : MonoBehaviour
     private void ReduceScale()
     {
         transform.localScale -= new Vector3(transform.localScale.x * reductionRate, transform.localScale.y * reductionRate, transform.localScale.z * reductionRate);
+        HitFlash();
         if (transform.localScale.x <= minRate)
         {
             Debug.Log("Chiquito, volver a juntar balas");
         }
+    }
+    private void HitFlash()
+    {
+        _spriteRenderer.color = Color.Lerp(_default, _hit, 0.5f);
+        Invoke(nameof(ReturnToNormalColor), 0.3f);
+    }
+
+    private void ReturnToNormalColor()
+    {
+        _spriteRenderer.color = _default;
     }
 }
