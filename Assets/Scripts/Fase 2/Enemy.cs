196ca102147c4e53057b9805a81ac14b3b353cd6
ArianaNChaves <navi.ariana99@gmail.com>
2024-08-16 23:11:36 -0300

    Balanced enemy scale

--- Assets/Scripts/Fase 2/Enemy.cs
+++ Assets/Scripts/Fase 2/Enemy.cs
@@ -2,13 +2,14 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private float reductionRate = 0.2f;
     [SerializeField] private float expandRate = 0.2f;
-    [SerializeField] private float maxRate = 7.0f;
-    [SerializeField] private float minRate = 2.0f;
+    [SerializeField] private float maxScaleRate = 7.0f;
+    [SerializeField] private float minScaleRate = 2.0f;
 
     private void OnEnable()
     {
@@ -26,7 +27,7 @@ public class Enemy : MonoBehaviour
         {
             Debug.Log("Hit Out!");
             transform.localScale += new Vector3(transform.localScale.x * expandRate, transform.localScale.y * expandRate, transform.localScale.z * expandRate);
-            if (transform.localScale.x >= maxRate)
+            if (transform.localScale.x >= maxScaleRate)
             {
                 Debug.Log("Perdiste");
             }
@@ -35,7 +36,7 @@ public class Enemy : MonoBehaviour
     private void MouthHit()
     {
         transform.localScale -= new Vector3(transform.localScale.x * reductionRate, transform.localScale.y * reductionRate, transform.localScale.z * reductionRate);
-        if (transform.localScale.x <= minRate)
+        if (transform.localScale.x <= minScaleRate)
         {
             Debug.Log("Ganaste");
         }
