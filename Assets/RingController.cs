2d7752e5ddbad1a99e88386cc571a2f96b6a2780
Joaquín Sacha Varisco <varisco54@gmail.com>
2024-08-17 15:33:29 -0300

    TransistionsAsset

--- Assets/RingController.cs
+++ Assets/RingController.cs
@@ -58,23 +58,26 @@ public class RingController : MonoBehaviour
         switch (randNum % 4)
         {
             case 0:
-                transform.localScale = new Vector2(1 * 0.21f, 1 * 0.21f);
+                transform.localScale = Vector2.Lerp(transform.localScale, new Vector2(1 * 0.21f, 1 * 0.21f), 1);
                 break;
             case 1:
-                transform.localScale = new Vector2(2 * 0.21f, 2 * 0.21f);
+                transform.localScale = Vector2.Lerp(transform.localScale, new Vector2(2 * 0.21f, 2 * 0.21f), 1);
+                //transform.localScale = new Vector2(2 * 0.21f, 2 * 0.21f);
                 break;
             case 2:
-                transform.localScale = new Vector2(3 * 0.21f, 3 * 0.21f);
+                transform.localScale = Vector2.Lerp(transform.localScale, new Vector2(3 * 0.21f, 3 * 0.21f), 1);
+                //transform.localScale = new Vector2(3 * 0.21f, 3 * 0.21f);
                 break;
             case 3:
-                transform.localScale = new Vector2(4 * 0.21f, 4 * 0.21f);
+                transform.localScale = Vector2.Lerp(transform.localScale, new Vector2(4 * 0.21f, 4 * 0.21f), 1);
+                //transform.localScale = new Vector2(4 * 0.21f, 4 * 0.21f);
                 break;
         }
         CanChange = true;
-        Debug.Log("Cambio " + randNum);
     }
 
-    private void PreChange(){
+    private void PreChange()
+    {
         Invoke(nameof(ChangeScale), 0.3f);
     }
 
